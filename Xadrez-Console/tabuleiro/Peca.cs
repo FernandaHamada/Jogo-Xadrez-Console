72a20ad88507df1a4301c3a18a0a07c6ad0241a2
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-09-18 08:41:14 -0300

    Implementando a verificação de xeque

--- Xadrez-Console/tabuleiro/Peca.cs
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -22,6 +22,11 @@ namespace tabuleiro
             qteMovimentos++;
         }
 
+        public void decrementarQdeMovimento()
+        {
+            qteMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis()
         {
             bool[,] mat = movimentosPossiveis();
